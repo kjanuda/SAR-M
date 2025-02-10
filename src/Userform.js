04474405e6f26a92b6bf4794e686fa74a8e572c6
kjanuda <janudakodi@gmail.com>
2025-02-10 15:41:07 +0530

    user

---
+++ src/Userform.js
@@ -0,0 +1,69 @@
+import { Grid, Input, Typography } from "@mui/material";
+
+const UseForm = (props) => {
+    return (
+        <Grid 
+            container
+            spacing={2}
+            sx={{
+                backgroundColor: '#ffffff',
+                marginBottom: '30px',
+                display: 'block',
+            }}
+        >
+            <Grid item xs={12}>
+                <Typography component={'h1'} sx={{ color: '#000000' }}>User Form</Typography>
+            </Grid>
+
+            <Grid item xs={12} sm={6} sx={{ display: 'flex' }}>
+                <Typography
+                    component={'label'}
+                    htmlFor="id"
+                    sx={{
+                        color: '#000000',
+                        marginRight: '20px',
+                        fontSize: '16px',
+                        width: '100px',
+                        display: 'block',
+                    }}
+                >
+                    ID
+                </Typography>
+                <Input
+                    type="number"
+                    id='id'
+                    name='id'
+                    sx={{ width: '400px' }}
+                    value={''}
+                    onChange={e => {}}
+                />
+            </Grid>
+
+            <Grid item xs={12} sm={6} sx={{ display: 'flex' }}>
+                <Typography
+                    component={'label'}
+                    htmlFor="name"
+                    sx={{
+                        color: '#000000',
+                        marginRight: '20px',
+                        fontSize: '16px',
+                        width: '100px',
+                        display: 'block',
+                    }}
+                >
+                    Name
+                </Typography>
+                <Input
+                    type="text"
+                    id='name'
+                    name='name'
+                    sx={{ width: '400px' }}
+                    value={''}
+                    onChange={e => {}}
+                />
+            </Grid>
+        </Grid>
+    );
+}
+
+export default UseForm;
