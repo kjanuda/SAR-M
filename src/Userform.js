d503da59f374096ec5c145a8c1abba1b62412ba6
kjanuda <janudakodi@gmail.com>
2025-02-12 00:19:31 +0530

    UTable

--- src/Userform.js
+++ src/Userform.js
@@ -1,21 +1,22 @@
-import { Grid, Input, Typography } from "@mui/material";
+import { Button, Grid2, Input, Typography } from "@mui/material"; // Use Grid2 instead of Grid
 
 const UseForm = (props) => {
     return (
-        <Grid 
+        <Grid2 
             container
             spacing={2}
             sx={{
                 backgroundColor: '#ffffff',
                 marginBottom: '30px',
-                display: 'block',
             }}
         >
-            <Grid item xs={12}>
-                <Typography component={'h1'} sx={{ color: '#000000' }}>User Form</Typography>
-            </Grid>
+            <Grid2 item xs={12}>
+                <Typography component={'h1'} sx={{ color: '#000000' }}>
+                    User Form
+                </Typography>
+            </Grid2>
 
-            <Grid item xs={12} sm={6} sx={{ display: 'flex' }}>
+            <Grid2 item xs={12} sm={6} sx={{ display: 'flex', alignItems: 'center' }}>
                 <Typography
                     component={'label'}
                     htmlFor="id"
@@ -24,22 +25,21 @@ const UseForm = (props) => {
                         marginRight: '20px',
                         fontSize: '16px',
                         width: '100px',
-                        display: 'block',
                     }}
                 >
                     ID
                 </Typography>
                 <Input
                     type="number"
-                    id='id'
-                    name='id'
+                    id="id"
+                    name="id"
                     sx={{ width: '400px' }}
                     value={''}
                     onChange={e => {}}
                 />
-            </Grid>
+            </Grid2>
 
-            <Grid item xs={12} sm={6} sx={{ display: 'flex' }}>
+            <Grid2 item xs={12} sm={6} sx={{ display: 'flex', alignItems: 'center' }}>
                 <Typography
                     component={'label'}
                     htmlFor="name"
@@ -48,21 +48,38 @@ const UseForm = (props) => {
                         marginRight: '20px',
                         fontSize: '16px',
                         width: '100px',
-                        display: 'block',
                     }}
                 >
                     Name
                 </Typography>
                 <Input
                     type="text"
-                    id='name'
-                    name='name'
+                    id="name"
+                    name="name"
                     sx={{ width: '400px' }}
                     value={''}
                     onChange={e => {}}
                 />
-            </Grid>
-        </Grid>
+            </Grid2>
+
+            <Grid2 item xs={12}>
+                <Button
+                    sx={{
+                        margin: 'auto',
+                        marginBottom: '20px',
+                        backgroundColor: '#00c6e6',
+                        color: '#000000',
+                        marginTop: '20px',
+                        '&:hover': {
+                            opacity: 0.7,
+                            backgroundColor: '#00c6e6',
+                        },
+                    }}
+                >
+                    Add
+                </Button>
+            </Grid2>
+        </Grid2>
     );
 }
 
---
+++ src/Users.js
@@ -0,0 +1,8 @@
+import UseForm from "./Userform";
+
+const Users = () => {
+    return (
+      <UseForm/>
+    );
+}
+export default Users;
