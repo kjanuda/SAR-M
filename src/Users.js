7ee13257dda843fd3bf2a49a5e3ee37aa891d16f
kjanuda <113584734+kjanuda@users.noreply.github.com>
2025-03-12 15:40:50 +0530

    Update Users.js

--- src/Users.js
+++ src/Users.js
@@ -1,1 +1,33 @@
 
+import { Box } from "@mui/material";
+import UseForm from "./Userform";
+import UserTable from "./UsersTable";
+
+const users = [
+  {
+    id: 1,
+    name: "januda",
+  },
+  {
+    id: 2,
+    name: "janandith",
+  },
+];
+
+const Users = () => {
+  return (
+    <Box
+      sx={{
+        width: "calc(100% - 100px)",
+        margin: "auto",
+        marginTop: "100px",
+      }}
+    >
+      <UseForm />
+      <UserTable rows={users} />
+    </Box>
+  );
+};
+
+export default Users;
+
